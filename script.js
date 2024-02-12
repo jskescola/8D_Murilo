6602300006d16243df3eda935923948bb2030cb1
murilo orias <mu.a.o@outlook.com>
2024-02-12 20:15:28 -0300

    melhora do codigo

---
+++ script.js
@@ -0,0 +1,25 @@
+function lucasfoto(){
+    window.location.href = "https://i.ibb.co/DLS5Ls6/IMG-20230707-102507-045.jpg";
+}
+function leonardofoto(){
+    window.location.href = "https://i.ibb.co/5G7kFkG/IMG-20231015-WA0003.jpg";
+}
+function bebedouro(){
+    window.location.href = "https://i.ibb.co/H7YzzT0/IMG-20231015-WA0002.jpg";
+}
+function quadrafoto(){
+    window.location.href = "https://s2-g1.glbimg.com/JU6UNLiC9XCSH3iZfhfwc9WwOpE=/0x0:640x356/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2018/N/p/LZmC6PSauBJV3sbvElqg/00325519.jpg";
+}
+function auditoriofoto(){
+    window.location.href = "https://i.ibb.co/h9qhHzj/Whats-App-Image-2023-10-17-at-11-45-08.jpg";
+}
+function salaartesfoto(){
+    window.location.href = "https://i.ibb.co/VjQGW3B/Whats-App-Image-2023-10-17-at-11-45-09.jpg";
+}
+function pedroimagem(){
+    window.location.href = "https://media.licdn.com/dms/image/C4E03AQEOxuBEbo3mHg/profile-displayphoto-shrink_800_800/0/1660069029163?e=2147483647&v=beta&t=2qK1wB1UMf0kfg1CKSVGXIz8F6WXXvhqEAyJ_LNXUy4";
+
+}
+function vergithub(){
+    window.location.href = "https://github.com/muriloorias";
+}
